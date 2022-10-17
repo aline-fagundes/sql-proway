eb2f6d2a896c3835824f2d2ea454c5bf0de53198
aline-fagundes <aline.martinsnapoli@gmail.com>
2022-10-17 13:09:47 -0300

    Adicionando script com deleçao de dados e tabelas

---
+++ 8- EXCLUIR DADOS E TABELAS.sql
@@ -0,0 +1,22 @@
+# 8- EXCLUIR DADOS E TABELAS
+
+delete from itens_pedidos;
+delete from pedidos;
+delete from clientes;
+delete from enderecos;
+delete from pagamentos;
+delete from produtos;
+delete from categorias;
+delete from marcas;
+
+
+
+drop table itens_pedidos;
+drop table pedidos;
+drop table clientes;
+drop table enderecos;
+drop table pagamentos;
+drop table produtos;
+drop table categorias;
+drop table marcas;
+
